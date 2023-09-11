d3cf345c5043ea586582b6e08245f7120e00c49c
Garrett Stewart <garrettbstewart@gmail.com>
2023-09-11 18:06:32 -0400

    added favicon

---
+++ js/main.js
@@ -0,0 +1,33 @@
+document.querySelector("button").addEventListener("click", getFetch);
+const iframe = document.querySelector("iframe");
+const img = document.querySelector("img");
+
+function getFetch() {
+  const choice = document.querySelector("input").value;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=indnPhDKN3xXETqcUDUwrKSnETefnW5chcIS4fDd`;
+
+  fetch(url + "&date=" + `${choice}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      img.src = null;
+      iframe.src = null;
+      switch (data.media_type) {
+        case "image":
+          img.style.display = "block";
+          img.src = data.hdurl;
+          iframe.style.display = "none";
+          break;
+        case "video":
+          iframe.style.display = "block";
+          iframe.src = data.url;
+          img.style.display = "none";
+          break;
+        default:
+          img.src = "/img/nasa.jpg";
+      }
+      document.querySelector("h3").innerText = data.title;
+      document.querySelector(".explanation").innerText = data.explanation;
+    })
+    .catch((err) => console.log(`error ${err}`));
+}
